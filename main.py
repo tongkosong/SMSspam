c73e8d65861dbe345897034c62609fec26da8a07
tongkosong <74802719+tongkosong@users.noreply.github.com>
2020-11-21 09:50:08 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+ (45 sloc)  1.02 KB
+ 
+import os,time,sys,shutil
+
+class Main:
+
+	def __init__(self):
+		self.detekos()
+
+	def menu(self):
+		print("""
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		;       S P A M  S M S      ;
+		;---------------------------;
+		;       Author : noobie     ;
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+NOTE: This tool's only work for Indonesia number phone.
+1. SMS Gratis
+""")
+		pilih=int(input('noobie/> '))
+		if pilih == 1:
+			import src.dbs
+		
+		else: print("[!] lihat menu dong(o)");self.menu()
+
+	def detekos(self):
+		#remove cache
+		try:
+			shutil.rmtree("src/__pycache__")
+		except: pass
+
+		if os.name in ['nt','win32']:
+			os.system('cls')
+		else: os.system('clear')
+		self.menu()
+
+try:
+	Main()
+except KeyboardInterrupt:
+	exit('[Exit] Key interrupt')
+except Exception as F:
+	print('Err: %s'%(F))
